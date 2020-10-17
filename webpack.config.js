c1c506a36860d57ad5769f166bc242c53614cfc3
Eneas Baltazar Mejia <ebaltazar@bside.com.mx>
2020-10-17 15:21:04 -0500

    Web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   entry: 'index.js',
   output: {
@@ -12,5 +14,16 @@ module.exports = {
         filename: 'index.html',
         template: 'index.html',
       }
-  )]
+  ),
+  new CopyPlugin({
+    patterns: [
+      { from: 'js', to: path.resolve(__dirname, './dist/js') },
+      { from: 'images', to: path.resolve(__dirname, './dist/images') },
+      { from: 'meta', to: path.resolve(__dirname, './dist/meta') },
+      { from: 'sounds', to: path.resolve(__dirname, './dist/sounds') },
+      { from: 'style', to: path.resolve(__dirname, './dist/style') },
+    ],
+  }),
+
+]
 }
